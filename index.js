06747d8e172014bfddbca8f7bdfe5c634ed2262f
Edward Viaene <ward.viaene@gmail.com>
2017-09-28 17:16:36 +0000

    v1.0.2 hello world

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World (v1.0.2)!');
 });
 
 var server = app.listen(3000, function () {
