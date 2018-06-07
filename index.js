6cc3e7e093083105bac0878c6ef3f3845f844909
edivina88 <38743184+edivina88@users.noreply.github.com>
2018-06-07 16:18:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World xxx (v1.0.2)!');
+  res.send('Hello World xxx (v1.0.3)!');
 });
 
 var server = app.listen(3000, function () {
