eb4a0a186d6676c32c7327f77f04887556e45321
Rishidha Gupta <57841979+rishidha@users.noreply.github.com>
2023-09-19 20:55:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app2 listening at http://%s:%s', host, port);
 });
