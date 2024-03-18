502cd09920474a5e9423370a9b9b64e9c7e0515a
silambarasanv79 <113892874+silambarasanv79@users.noreply.github.com>
2024-03-18 14:57:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+console.log("hi, it's silambarasan")
